156ddb9591c1ac9bb48c73001d2d3156f6ab7fd9
Radovan Kepák <radovan.kepak@mallgroup.com>
2021-08-12 15:41:31 +0200

    Initial commit

---
+++ src/Bootstrap/Configurator.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Mallgroup\Bootstrap;
+
+use Mallgroup\DI\Adapters\EnvironmentAdapter;
+use Nette\Bootstrap;
+use Nette\DI\Config\Loader;
+
+class Configurator extends Bootstrap\Configurator
+{
+    private Loader $loader;
+
+    public function createLoader(): Loader
+    {
+        $this->loader ??= parent::createLoader();
+        return $this->loader;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->createLoader()->addAdapter('env', EnvironmentAdapter::class);
+    }
+}
