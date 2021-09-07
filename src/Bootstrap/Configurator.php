14dfc007e59afe146d339ac3047dc94bba555438
Radovan Kepák <radovan.kepak@mallgroup.com>
2021-09-07 10:51:40 +0200

    Configurator: add default parameter resolution so appDir is correct

--- src/Bootstrap/Configurator.php
+++ src/Bootstrap/Configurator.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Mallgroup\Bootstrap;
 
+use Composer\Autoload\ClassLoader;
 use Mallgroup\DI\Config\Adapters\EnvironmentAdapter;
 use Nette\Bootstrap;
 use Nette\DI\Config\Loader;
@@ -15,4 +16,22 @@ class Configurator extends Bootstrap\Configurator
         $loader->addAdapter('env', EnvironmentAdapter::class);
         return $loader;
     }
+
+	protected function getDefaultParameters(): array
+	{
+		$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+		$last = end($trace);
+		$debugMode = static::detectDebugMode();
+		$loaderRc = class_exists(ClassLoader::class)
+			? new \ReflectionClass(ClassLoader::class)
+			: null;
+		return [
+			'appDir' => isset($trace[2]['file']) ? dirname($trace[2]['file']) : null,
+			'wwwDir' => isset($last['file']) ? dirname($last['file']) : null,
+			'vendorDir' => $loaderRc ? dirname($loaderRc->getFileName(), 2) : null,
+			'debugMode' => $debugMode,
+			'productionMode' => !$debugMode,
+			'consoleMode' => PHP_SAPI === 'cli',
+		];
+	}
 }
