ef1494f3e18fab14be404ef72a1d0ac3fe5b1f0e
Radovan Kepák <radovan.kepak@mallgroup.com>
2021-09-14 13:41:18 +0200

    Update Configurator.php

--- src/Bootstrap/Configurator.php
+++ src/Bootstrap/Configurator.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Mallgroup\Bootstrap;
 
-use Composer\Autoload\ClassLoader;
 use Mallgroup\DI\Config\Adapters\EnvironmentAdapter;
 use Nette\Bootstrap;
 use Nette\DI\Config\Loader;
+use function debug_backtrace;
 
 class Configurator extends Bootstrap\Configurator
 {
@@ -17,21 +17,17 @@ class Configurator extends Bootstrap\Configurator
         return $loader;
     }
 
+
+	/**
+	 * @return array<mixed>
+	 */
 	protected function getDefaultParameters(): array
 	{
 		$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-		$last = end($trace);
-		$debugMode = static::detectDebugMode();
-		$loaderRc = class_exists(ClassLoader::class)
-			? new \ReflectionClass(ClassLoader::class)
-			: null;
-		return [
-			'appDir' => isset($trace[2]['file']) ? dirname($trace[2]['file']) : null,
-			'wwwDir' => isset($last['file']) ? dirname($last['file']) : null,
-			'vendorDir' => $loaderRc ? dirname($loaderRc->getFileName(), 2) : null,
-			'debugMode' => $debugMode,
-			'productionMode' => !$debugMode,
-			'consoleMode' => PHP_SAPI === 'cli',
-		];
+
+		$parameters = parent::getDefaultParameters();
+		$parameters['appDir'] = dirname((string)$trace[2]['file']);
+
+		return $parameters;
 	}
 }
